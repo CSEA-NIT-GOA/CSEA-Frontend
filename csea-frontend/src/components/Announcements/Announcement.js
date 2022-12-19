771495fdb84b405f7305ba0c077892b070776de3
Aryan Shrivastav <aryanshr91003@gmail.com>
2022-12-19 21:12:39 +0530

    added Anouncement section

---
+++ csea-frontend/src/components/Announcements/Announcement.js
@@ -0,0 +1,68 @@
+import Carousel from "react-spring-3d-carousel";
+import { useState, useEffect } from "react";
+import { config } from "react-spring";
+
+function Announcement(props) {
+  const table = props.cards.map((element, index) => {
+    return { ...element, onClick: () => setGoToSlide(index) };
+  });
+
+  const [offsetRadius, setOffsetRadius] = useState(2);
+  const [showArrows, setShowArrows] = useState(false);
+  const [goToSlide, setGoToSlide] = useState(null);
+  const [cards] = useState(table);
+
+  useEffect(() => {
+    setOffsetRadius(props.offset);
+    setShowArrows(props.showArrows);
+  }, [props.offset, props.showArrows]);
+
+  return (
+    <div
+      // style={{ width: props.width, height: props.height, margin: props.margin }}
+      className="flex justify-evenly max-[850px]:flex-col"
+    >
+      <div className="h-[500px] w-1/4 mx-32 mb-20 max-[850px]:mx-auto">
+
+        <h1 className="text-xl font-bold pb-8 text-center ">Upcomming Events</h1>
+        <Carousel
+          slides={cards}
+          goToSlide={goToSlide}
+          offsetRadius={offsetRadius}
+          showNavigation={showArrows}
+          animationConfig={config.gentle}
+        />
+      </div>
+      <div>
+        <div className="w-auto">
+          <h1 className="text-xl font-bold pb-8 text-center max-[850px]:pt-8">Announcements</h1>
+          <table class="table-fixed max-[850px]:mx-auto">
+            <thead>
+              <tr>
+                <th></th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Announcement 1</td>
+                <td>lorem ipsum</td>
+              </tr>
+              <tr>
+                <td>Announcement 2</td>
+                <td>lorem ipsum</td>
+              </tr>
+              <tr>
+                <td>Announcement 3</td>
+                <td>lorem ipsum</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+
+export default Announcement;
--- csea-frontend/src/components/pages/Home.js
+++ csea-frontend/src/components/pages/Home.js
@@ -5,6 +5,45 @@ import '../HeroSection'
 import Herosection from '../HeroSection'
 import '../../index.css'
 import Animation from '../animation'
+import Announcement from '../Announcements/Announcement'
+import Card from '../Announcements/Card'
+
+import { v4 as uuidv4 } from "uuid";
+
+let cards = [
+  {
+    key: uuidv4(),
+    content: (
+      <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/convertplus_thumbnail.jpg" />
+    )
+  },
+  {
+    key: uuidv4(),
+    content: (
+      <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/acf_pro.png" />
+    )
+  },
+  {
+    key: uuidv4(),
+    content: (
+      <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2017/12/layer_slider_plugin_thumb.png" />
+    )
+  },
+  {
+    key: uuidv4(),
+    content: (
+      <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2016/08/slider_revolution-1.png" />
+    )
+  },
+  {
+    key: uuidv4(),
+    content: (
+      <Card imagen="https://updates.theme-fusion.com/wp-content/uploads/2019/01/pwa_880_660.jpg" />
+    )
+  }
+];
+
+
 function Home() {
   return (
     <div>
@@ -13,7 +52,14 @@ function Home() {
         <Animation/>
         <Gallery/>
       </div>
-      
+      <Announcement
+        cards={cards}
+        // height="500px"
+        // width="30%"
+        // margin="2rem 9rem 5rem"
+        offset={2}
+        showArrows={false}
+      /> 
       <Footer/>
     </div>
   )
