9862e3b12840b7a906c31f56a5bee163a5e3c9fa
Omkar Borker <omkarborker15@gmail.com>
2022-12-21 13:05:02 +0530

    new

--- csea-frontend/src/components/pages/Home.js
+++ csea-frontend/src/components/pages/Home.js
@@ -7,15 +7,14 @@ import '../../index.css'
 import Animation from '../animation'
 function Home() {
   return (
-    <div>
+    <>
       <Herosection/>
       <div className='p-4 my-2 flex-auto justify-items-center'>
         <Animation/>
         <Gallery/>
-      </div>
-      
+      </div>  
       <Footer/>
-    </div>
+    </>
   )
 }
 
