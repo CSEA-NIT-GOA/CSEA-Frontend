0e95908399476ee1040b469ecc7c81289bac3e9d
Aryan Shrivastav <aryanshr91003@gmail.com>
2022-12-24 09:28:35 +0530

    improved responsiveness

--- csea-frontend/src/components/event/event.js
+++ csea-frontend/src/components/event/event.js
@@ -5,7 +5,7 @@ function Event() {
     <div className='h-auto'>
     <section id="section06" class="demo">
       <a href="#"><span></span>Scroll</a>
-      <h1>Scroll Down Button #6</h1>
+      <h1 className='text-6xl max-md:text-4xl'>Scroll Down Button #6</h1>
     </section>
   </div>
   )
