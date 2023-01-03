78ce1c93ceca200e1a2c0ef5ef9d6c23157388ae
Tushar Pal <99737148+sloth0072@users.noreply.github.com>
2023-01-03 17:06:30 +0530

    Update Footer.js
    Color scheme and reduced few options.
    Newsletter option left as discussed in meet.

--- csea-frontend/src/components/Footer.js
+++ csea-frontend/src/components/Footer.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
+
 function Footer() {
     return (
-        <footer aria-label="Site Footer" class="bg-slate-800">
+        <footer aria-label="Site Footer" class="bg-[#152351]">
             <div class="mx-auto max-w-5xl px-4 py-14 sm:px-6 lg:px-8">
-                <div class="mx-auto max-w-md">
+            <div class="mx-auto max-w-md">
                     <strong
-                        class="block text-center text-xl font-medium text-slate-50 sm:text-3xl"
+                        class="block text-center text-xl font-medium text-[#F5F5F5] sm:text-3xl"
                     >
                         Want us to email you with the latest blockbuster news?
                     </strong>
@@ -31,6 +32,8 @@ function Footer() {
                         </div>
                     </form>
                 </div>
+                
+                {/* logo
                 <div class="flex justify-center text-teal-400 pt-4">
                     <svg
                         class="h-8"
@@ -79,36 +82,36 @@ function Footer() {
                             fill="currentColor"
                         />
                     </svg>
-                </div>
+    </div> */}
 
-                <p class="mx-auto mt-4 max-w-md text-center leading-relaxed text-gray-300">
-                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Incidunt
-                    consequuntur amet culpa cum itaque neque.
+
+
+                <p class="mx-auto mt-4 max-w-md text-center leading-relaxed text-[#FFD4B2]">
+                    Footer content if any
                 </p>
 
                 <nav aria-label="Footer Nav" class="mt-6">
-                    <ul class="flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12">
+                    <ul class="flex flex-wrap justify-center gap-6 md:gap-8 lg:gap-12 text-xl text-[#FB2576]">
                         <li>
-                            <a class="text-gray-50 transition hover:text-gray-50/75" href="/">
+                            <a class=" transition hover:text-gray-50/75" href="/">
                                 About
                             </a>
                         </li>
 
                         <li>
-                            <a class="text-gray-50 transition hover:text-gray-50/75" href="/">
-                                Services
-                            </a>
-                        </li>
-
-                        <li>
-                            <a class="text-gray-50 transition hover:text-gray-50/75" href="/">
+                            <a class=" transition hover:text-gray-50/75" href="/">
                                 Events
                             </a>
                         </li>
 
                         <li>
-                            <a class="text-gray-50 transition hover:text-gray-50/75" href="/">
-                                Blog
+                            <a class=" transition hover:text-gray-50/75" href="/">
+                                Contact
+                            </a>
+                        </li>
+                        <li>
+                            <a class=" transition hover:text-gray-50/75" href="/">
+                                Developers
                             </a>
                         </li>
                     </ul>
@@ -120,7 +123,7 @@ function Footer() {
                             href="/"
                             rel="noreferrer"
                             target="_blank"
-                            class="text-gray-50 transition hover:text-gray-50/75"
+                            class="text-blue-500 transition hover:text-gray-50/75"
                         >
                             <span class="sr-only">Facebook</span>
                             <svg
@@ -138,35 +141,13 @@ function Footer() {
                         </a>
                     </li>
 
+                    
                     <li>
                         <a
                             href="/"
                             rel="noreferrer"
                             target="_blank"
-                            class="text-gray-50 transition hover:text-gray-50/75"
-                        >
-                            <span class="sr-only">Instagram</span>
-                            <svg
-                                class="h-8 w-8"
-                                fill="currentColor"
-                                viewBox="0 0 24 24"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                                    clip-rule="evenodd"
-                                />
-                            </svg>
-                        </a>
-                    </li>
-
-                    <li>
-                        <a
-                            href="/"
-                            rel="noreferrer"
-                            target="_blank"
-                            class="text-gray-50 transition hover:text-gray-50/75"
+                            class="text-blue-400 transition hover:text-gray-50/75"
                         >
                             <span class="sr-only">Twitter</span>
                             <svg
@@ -187,7 +168,7 @@ function Footer() {
                             href="/"
                             rel="noreferrer"
                             target="_blank"
-                            class="text-gray-50 transition hover:text-gray-50/75"
+                            class="text-gray-1000 transition hover:text-gray-50/75"
                         >
                             <span class="sr-only">GitHub</span>
                             <svg
@@ -205,28 +186,6 @@ function Footer() {
                         </a>
                     </li>
 
-                    <li>
-                        <a
-                            href="/"
-                            rel="noreferrer"
-                            target="_blank"
-                            class="text-gray-50 transition hover:text-gray-50/75"
-                        >
-                            <span class="sr-only">Dribbble</span>
-                            <svg
-                                class="h-8 w-8"
-                                fill="currentColor"
-                                viewBox="0 0 24 24"
-                                aria-hidden="true"
-                            >
-                                <path
-                                    fill-rule="evenodd"
-                                    d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
-                                    clip-rule="evenodd"
-                                />
-                            </svg>
-                        </a>
-                    </li>
                 </ul>
             </div>
         </footer>
