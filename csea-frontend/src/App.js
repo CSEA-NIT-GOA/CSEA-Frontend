e717944884899b78ad7da5c11c8f757cb4a317fc
Niranjan <niranjanhebli77@gmail.com>
2023-02-14 00:42:04 +0530

    Seperate Newsletter created

--- csea-frontend/src/App.js
+++ csea-frontend/src/App.js
@@ -2,16 +2,15 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './components/pages/Home';
 import FullTeam from './components/Announcements/FullTeam';
-import Herosection from './components/HeroSection';
-import Navbar from './components/Navbar';
+import Newsletter from './components/newsletter/Newsletter';
+
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Herosection/>
       <Routes>
       <Route path='/' exact element={ <Home /> }/>
       <Route path='/Team' element={ <FullTeam /> }/>
+      <Route path='/Newsletter' element={<Newsletter/>}/>
       </Routes>
     </Router>
   );
