f94e403cbc2d61629cb47cb38a3308879cd51f9b
Omkar Borker <omkarborker15@gmail.com>
2023-03-18 11:13:47 +0530

    modified:   csea-frontend/src/components/utilities/Gallery.js

--- csea-frontend/src/components/utilities/Gallery.js
+++ csea-frontend/src/components/utilities/Gallery.js
@@ -1,39 +1,106 @@
-import React from 'react'
+// import React from 'react'
 
-function Gallery() {
-  return (
-    // <div className="carousel my-10 h-96 w-full py-6">
-    <div className="carousel h-96 pt-10" id='gallery'>
-    <div id="slide1" className="carousel-item relative w-full">
-      <img src="https://placeimg.com/800/200/arch" className="w-full" alt="gallery" />
-      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide4" className="btn btn-circle">❮</a> 
-        <a href="#slide2" className="btn btn-circle">❯</a>
+// function Gallery() {
+//   return (
+//     // <div className="carousel my-10 h-96 w-full py-6">
+//     <div className="carousel h-96 pt-10" id='gallery'>
+//     <div id="slide1" className="carousel-item relative w-full">
+//       <img src="https://placeimg.com/800/200/arch" className="w-full" alt="gallery" />
+//       <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+//         <a href="#slide4" className="btn btn-circle">❮</a> 
+//         <a href="#slide2" className="btn btn-circle">❯</a>
+//       </div>
+//     </div> 
+//     <div id="slide2" className="carousel-item relative w-full">
+//       <img src="https://placeimg.com/800/200/arch" className="w-full" alt="gallery" />
+//       <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+//         <a href="#slide1" className="btn btn-circle">❮</a> 
+//         <a href="#slide3" className="btn btn-circle">❯</a>
+//       </div>
+//     </div> 
+//     <div id="slide3" className="carousel-item relative w-full">
+//       <img src="https://placeimg.com/800/200/arch" className="w-full" alt="gallery" />
+//       <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+//         <a href="#slide2" className="btn btn-circle">❮</a> 
+//         <a href="#slide4" className="btn btn-circle">❯</a>
+//       </div>
+//     </div> 
+//     <div id="slide4" className="carousel-item relative w-full">
+//       <img src="https://placeimg.com/800/200/arch" className="w-full" alt="gallery" />
+//       <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+//         <a href="#slide3" className="btn btn-circle">❮</a> 
+//         <a href="#slide1" className="btn btn-circle">❯</a>
+//       </div>
+//     </div>
+//   </div>
+//   )
+// }
+
+// export default Gallery
+
+import React, { Component } from "react";
+import Slider from "react-slick";
+import './gallery.css'
+
+class CustomSlide extends Component {
+  render() {
+    const { index, ...props } = this.props;
+    return (
+      <div {...props}>
+        <img src = {index}/>
       </div>
-    </div> 
-    <div id="slide2" className="carousel-item relative w-full">
-      <img src="https://placeimg.com/800/200/arch" className="w-full" alt="gallery" />
-      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide1" className="btn btn-circle">❮</a> 
-        <a href="#slide3" className="btn btn-circle">❯</a>
-      </div>
-    </div> 
-    <div id="slide3" className="carousel-item relative w-full">
-      <img src="https://placeimg.com/800/200/arch" className="w-full" alt="gallery" />
-      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide2" className="btn btn-circle">❮</a> 
-        <a href="#slide4" className="btn btn-circle">❯</a>
-      </div>
-    </div> 
-    <div id="slide4" className="carousel-item relative w-full">
-      <img src="https://placeimg.com/800/200/arch" className="w-full" alt="gallery" />
-      <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
-        <a href="#slide3" className="btn btn-circle">❮</a> 
-        <a href="#slide1" className="btn btn-circle">❯</a>
-      </div>
-    </div>
-  </div>
-  )
+    );
+  }
 }
 
-export default Gallery
+
+export default class Responsive extends Component {
+  render() {
+    var settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      initialSlide: 0,
+      responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 600,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 2,
+            initialSlide: 2
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    };
+    return (
+      <div>
+        <Slider {...settings}>
+           <CustomSlide index={"https://picsum.photos/id/10/600"} />
+          <CustomSlide index={"https://picsum.photos/id/20/600"} />
+          <CustomSlide index={"https://picsum.photos/id/30/600"} />
+          <CustomSlide index={"https://picsum.photos/id/40/600"} />
+          <CustomSlide index={"https://picsum.photos/id/50/600"} />
+          <CustomSlide index={"https://picsum.photos/id/60/600"} />
+        </Slider>
+      </div>
+    );
+  }
+}
