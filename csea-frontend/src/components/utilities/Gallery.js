3562365a3cb0aed9c9ad43d507092f5a7c1c4e9b
Omkar Borker <omkarborker15@gmail.com>
2023-03-18 11:15:35 +0530

    modified:   csea-frontend/src/components/utilities/Gallery.js

--- csea-frontend/src/components/utilities/Gallery.js
+++ csea-frontend/src/components/utilities/Gallery.js
@@ -60,15 +60,15 @@ export default class Responsive extends Component {
       dots: true,
       infinite: true,
       speed: 500,
-      slidesToShow: 1,
-      slidesToScroll: 1,
+      slidesToShow: 3,
+      slidesToScroll: 3,
       initialSlide: 0,
       responsive: [
         {
           breakpoint: 1024,
           settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1,
+            slidesToShow: 3,
+            slidesToScroll: 3,
             infinite: true,
             dots: true
           }
@@ -76,16 +76,16 @@ export default class Responsive extends Component {
         {
           breakpoint: 600,
           settings: {
-            slidesToShow: 2,
-            slidesToScroll: 2,
-            initialSlide: 2
+            slidesToShow: 3,
+            slidesToScroll: 3,
+            initialSlide: 3
           }
         },
         {
           breakpoint: 480,
           settings: {
-            slidesToShow: 1,
-            slidesToScroll: 1
+            slidesToShow: 3,
+            slidesToScroll: 3
           }
         }
       ]
